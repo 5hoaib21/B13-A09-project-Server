dbba3fa459dac238ea245427addf6a337ff2bb85
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-05-20 01:31:04 +0600

    create delete api

--- index.js
+++ index.js
@@ -1,71 +1,80 @@
 const express = require("express");
-const dotenv = require('dotenv')
-const cors = require('cors')
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-dotenv.config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+dotenv.config();
 const uri = process.env.MONGODB_URI;
 
 const app = express();
 const PORT = process.env.PORT;
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
     await client.connect();
-   
-    const db = client.db("studyNook")
-    const roomCollection = db.collection('rooms')
 
-    app.get('/room', async (req, res) => {
-      const result = await roomCollection.find().toArray()
-      res.json(result)
-    })
+    const db = client.db("studyNook");
+    const roomCollection = db.collection("rooms");
 
+    app.get("/room", async (req, res) => {
+      const result = await roomCollection.find().toArray();
+      res.json(result);
+    });
 
-    app.post('/room',async (req, res) => {
-      const roomData = req.body
+    app.post("/room", async (req, res) => {
+      const roomData = req.body;
       console.log(roomData);
-      const result = await roomCollection.insertOne(roomData)
+      const result = await roomCollection.insertOne(roomData);
+      res.json(result);
+    });
+
+    app.get("/room/:id", async (req, res) => {
+      const { id } = req.params;
+
+      const result = await roomCollection.findOne({ _id: new ObjectId(id) });
+
+      res.json(result);
+    });
+
+    app.patch("/room/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedData = req.body;
+
+      const result = await roomCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
+      );
+      res.json(result);
+    });
+
+    app.delete("/room/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await roomCollection.deleteOne({ _id: new ObjectId(id) });
       res.json(result)
-    })
-
-
-    app.get("/room/:id", async (req,res) => {
-      const {id} = req.params;
-
-      const result = await roomCollection.findOne({ _id: new ObjectId(id) })
-
-      res.json(result)
-    })
-
-
-
-
-
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
-
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-  res.send('server is running fine!!!!')
+  res.send("server is running fine!!!!");
 });
 
 app.listen(PORT, () => {
